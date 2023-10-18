3d1c202d7abae7559ef99999862c4771f3e9a7d5
MahdisRezaei2 <mahdis.re20@gmail.com>
2023-10-17 22:15:07 -0700

    I add pictures for the portfoli part, Menu.jsx, Portfolio.jsx

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,35 +1,36 @@
-import Work1 from "../../assests/work-1.svg";
-
-const Data =[
-{
-    id:1,
-    image:Project1,
-    title:"Full Calendar and Schedular React Web Application",
+import Work1 from "../../assests/work-1.png";
+import Work2 from "../../assests/work-2.png";
+import Work3 from "../../assests/work-3.png";
+import Work4 from "../../assests/work-4.png";
+import React from "react";
+const Data = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Full Calendar and Schedular React Web Application",
     category: "Web",
-},
+  },
 
-{
-    id:2,
-    image:Project2,
-    title:"To-Do List Web Application",
-    category: "Web2",
-},
-
-{
-    id:3,
-    image:Project3,
-    title:"Database Web Application for Real State Management",
+  {
+    id: 2,
+    image: Work2,
+    title: "Database Web Application for Real State Management ",
     category: "Database",
-},
+  },
 
-{
-    id:4,
-    image:Project4,
-    title:"Fitness Goal Tracker Web Application",
+  {
+    id: 3,
+    image: Work3,
+    title: "To-Do List Web Application",
+    category: "Web2",
+  },
+
+  {
+    id: 4,
+    image: Work4,
+    title: "Fitness Goal Tracker Web Application",
     category: "web3",
-},
-
+  },
 ];
-
 
 export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
+import Menu from "./Menu";
 
 const Portfolio = () => {
+  const [items, setItems]= useState(Menu);
   return (
+
     <section className="work container section" id="work">
       <h2 className="section__title">Projects</h2>
 
@@ -25,7 +28,25 @@ const Portfolio = () => {
         </span>
       </div>
 
-      <div className="work__container grid"></div>
+      <div className="work__container grid">
+        {items.map((elem) => {
+          const{id, image, title, category}= elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="" className="work__img"/>
+                <div className="work__mask"></div>
+              </div>
+
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href="#" className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
+            </div>
+          )
+        })}
+      </div>
     </section>
   );
 };
